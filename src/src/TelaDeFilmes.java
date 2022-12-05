91e65c48585842d24f9575eb45f6b1fc9cc4c6ab
Luciano bruno <luciaanobruno@gmail.com>
2022-12-04 22:57:47 -0300

    Projeto LPOO

---
+++ src/src/TelaDeFilmes.java
@@ -0,0 +1,180 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class TelaDeFilmes extends JFrame {
+
+    public TelaDeFilmes(){
+
+        //CONTAINER PRINCIPAL
+        JPanel principal = new JPanel();
+        principal.setPreferredSize(new Dimension(1000,1500));
+        principal.setLayout(null);
+        add(principal);
+        pack();
+
+        //BARRA DE ROLAGEM DE TELA
+        JScrollPane scroll = new JScrollPane(principal);
+        add(scroll);
+
+        //CONFIGURAÇÕES INICIAIS
+
+        setVisible(true);
+        //setSize(1000,800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setLayout(null);
+
+        //---------------------------LABELS-------------------------------------//
+
+        //LABEL "EM EXIBIÇÃO"
+        JLabel lblexibicao = new JLabel("EM EXIBIÇÃO");
+        lblexibicao.setFont(new Font("arial",Font.BOLD,20));
+        lblexibicao.setBounds(20,50,700,200);
+        principal.add(lblexibicao);
+
+        //LABEL "CATÁLOGO DE FILMES"
+        JLabel lblcatalogo = new JLabel("CATÁLOGO DE FILMES");
+        lblcatalogo.setFont(new Font("arial",Font.BOLD,30));
+        lblcatalogo.setBounds(310,0,400,50);
+        lblcatalogo.setForeground(new Color(63, 154, 13, 236));
+        principal.add(lblcatalogo);
+
+
+        //LABEL "EM BREVE"
+        JLabel lblembreve = new JLabel("EM BREVE:");
+        lblembreve.setFont(new Font("arial",Font.BOLD,20));
+        lblembreve.setBounds(20,1000,700,200);
+        principal.add(lblembreve);
+
+
+
+
+        //-------------------------BOTÕES--------------------------------------//
+
+
+        //BOTÃO FILME 1
+        JButton botaof1 = new JButton("FILME 1");
+        botaof1.setFont(new Font("arial",Font.BOLD,15));
+        botaof1.setBounds(70,200,200,250);
+        principal.add(botaof1);
+
+        //BOTAO FILME 2
+        JButton botaof2 = new JButton("FILME 2");
+        botaof2.setFont(new Font("arial",Font.BOLD,15));
+        botaof2.setBounds(370,200,200,250);
+        principal.add(botaof2);
+
+        //BOTAO FILME 3
+        JButton botaof3 = new JButton("FILME 3");
+        botaof3.setFont(new Font("arial",Font.BOLD,15));
+        botaof3.setBounds(670,200,200,250);
+        principal.add(botaof3);
+
+        //BOTAO FILME 4
+        JButton botaof4 = new JButton("FILME 3");
+        botaof4.setFont(new Font("arial",Font.BOLD,15));
+        botaof4.setBounds(70,500,200,250);
+        principal.add(botaof4);
+
+        //BOTAO FILME 5
+        JButton botaof5 = new JButton("FILME 5");
+        botaof5.setFont(new Font("arial",Font.BOLD,15));
+        botaof5.setBounds(370,500,200,250);
+        principal.add(botaof5);
+
+        //BOTAO FILME 6
+        JButton botaof6 = new JButton("FILME 6");
+        botaof6.setFont(new Font("arial",Font.BOLD,15));
+        botaof6.setBounds(670,500,200,250);
+        principal.add(botaof6);
+
+        //BOTAO FILME 7
+        JButton botaof7 = new JButton("FILME 7");
+        botaof7.setFont(new Font("arial",Font.BOLD,15));
+        botaof7.setBounds(70,800,200,250);
+        principal.add(botaof7);
+
+        //BOTAO FILME 8
+        JButton botaof8 = new JButton("FILME 8");
+        botaof8.setFont(new Font("arial",Font.BOLD,15));
+        botaof8.setBounds(370,800,200,250);
+        principal.add(botaof8);
+
+        //BOTAO FILME 9
+        JButton botaof9 = new JButton("FILME 9");
+        botaof9.setFont(new Font("arial",Font.BOLD,15));
+        botaof9.setBounds(670,800,200,250);
+        principal.add(botaof9);
+
+        //BOTAO VOLTAR
+        JButton botaovoltar = new JButton("VOLTAR");
+        botaovoltar.setFont(new Font("arial",Font.BOLD,10));
+        botaovoltar.setBounds(20,10,90,30);
+        principal.add(botaovoltar);
+
+        //BOTAO FILME 1 EM BREVE
+        JButton embreve1 = new JButton("em breve 1");
+        embreve1.setFont(new Font("arial",Font.BOLD,10));
+        embreve1.setBounds(70,1150,200,250);
+        principal.add(embreve1);
+
+        //BOTAO FILME 2 EM BREVE
+        JButton embreve2 = new JButton("em breve 2");
+        embreve2.setFont(new Font("arial",Font.BOLD,10));
+        embreve2.setBounds(370,1150,200,250);
+        principal.add(embreve2);
+
+        //BOTAO FILME 3 EM BREVE
+        JButton embreve3 = new JButton("em breve 3");
+        embreve3.setFont(new Font("arial",Font.BOLD,10));
+        embreve3.setBounds(670,1150,200,250);
+        principal.add(embreve3);
+
+
+        //-----------------FUNÇÃO BOTOES--------------------------------//
+
+        botaovoltar.addActionListener(this::voltar);
+
+
+
+
+        //--------------CAIXINHA PARA BUSCAR FILMES COM CONTAINER------------//
+
+
+        JComboBox<String> buscarfilme = new JComboBox<>();
+        JPanel painel = new JPanel();
+        painel.setBounds(330,100,300,70);
+        painel.add(buscarfilme);
+        principal.add(painel);
+
+        buscarfilme.addItem("BUSCAR FILME");
+        buscarfilme.addItem("filme 1");
+        buscarfilme.addItem("filme 2");
+        buscarfilme.addItem("filme 3");
+        buscarfilme.addItem("filme 4");
+        buscarfilme.addItem("filme 5");
+        buscarfilme.addItem("filme 6");
+        buscarfilme.addItem("filme 7");
+        buscarfilme.addItem("filme 8");
+        buscarfilme.addItem("filme 9");
+        buscarfilme.setSelectedIndex(0);
+        buscarfilme.setBounds(400,100,180,50);
+
+
+
+
+    }
+    //---------------MÉTODOS PARA ATRIBUIR AÇÃO AOS BOTÕES----------------------//
+    private void voltar(ActionEvent actionEvent) {
+        TelaInicial voltar = new TelaInicial();
+        voltar.setVisible(true);
+        this.dispose();
+
+    }
+
+
+}
---
+++ src/src/TelaInicial.java
@@ -0,0 +1,109 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+
+
+
+public class TelaInicial extends JFrame  {
+
+
+    //DEFININDO IMAGEM DO CANTO SUPERIOR ESQUERDO
+    ImageIcon logo_ifpe = new ImageIcon(getClass().getResource("ifpe.png"));
+    JLabel ifpe = new JLabel(logo_ifpe);
+
+    public TelaInicial(){
+
+        //CONFIGURAÇÕES INICIAIS
+
+        setVisible(true);
+        setTitle("cineIF PAULISTA");
+        setSize(1000,800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setLayout(null);
+
+
+        //---------------------- LABELS ---------------------------------//
+
+        //TITULO
+        JLabel titulo = new JLabel("cineIF PAULISTA");
+        titulo.setFont(new Font("Arial",Font.BOLD,40));
+        titulo.setBounds(330,10,500,100);
+        titulo.setForeground(new Color(63, 164, 13, 236));
+        add(titulo);
+
+        //FILMES DESTAQUES
+        JLabel destaques = new JLabel("EM DESTAQUE");
+        destaques.setFont(new Font("Arial",Font.BOLD,20));
+        destaques.setBounds(430,180,300,100);
+        destaques.setForeground(new Color(10,10,10));
+        add(destaques);
+
+
+        //----------------------BOTOES----------------------------------//
+
+        //BOTAO VER TODOS OS FILMES
+        JButton todos_os_filmes = new JButton("Ver todos os filmes");
+        todos_os_filmes.setBounds(790,150,150,30);
+        todos_os_filmes.setFont(new Font("arial",Font.BOLD,12));
+        //todos_os_filmes.setForeground(new Color(55, 236, 14));
+        //todos_os_filmes.setBackground(new Color(14, 7, 7));
+        add(todos_os_filmes);
+
+        //BOTÃO FILME 1
+        JButton filme1 = new JButton("FILME 1");
+        filme1.setBounds(110,310,200,250);
+        add(filme1);
+
+        //BOTAO FILME 2
+        JButton filme2 = new JButton("FILME 2");
+        filme2.setBounds(410,310,200,250);
+        add(filme2);
+
+        //BOTAO FILME 3
+        JButton filme3 = new JButton("FILME 3");
+        filme3.setBounds(710,310,200,250);
+        add(filme3);
+
+        //BOTAO ADMINISTRADOR
+        JButton adm = new JButton("Administrador");
+        adm.setBounds(790,650,150,30);
+        adm.setFont(new Font("arial",Font.BOLD,15));
+        //adm.setForeground(new Color(69, 232, 31, 255));
+        //adm.setBackground(new Color(10,10,10));
+        add(adm);
+
+
+        //-------------FUNÇÃO DOS BOTÕES------------------------------//
+
+        //FUNÇÃO BOTÃO "VER TODOS OS FILMES"
+        todos_os_filmes.addActionListener(this::verfilmes);
+
+        //FUNÇÃO BOTÃO "ADMINISTRADOR"
+        adm.addActionListener(this::loginadm);
+
+
+        //--------------------------IMAGENS-------------------------------//
+        add(ifpe);
+        ifpe.setBounds(0,0,300,300);
+
+    }
+
+    //METODO QUE ATRIBUI AÇÃO AO BOTÃO DO ADMINISTRADOR
+    private void loginadm(ActionEvent actionEvent) {
+
+    }
+
+
+    //METODO QUE ATRIBUI AÇÃO AO BOTÃO "VER FILMES"
+    private void verfilmes(ActionEvent actionEvent) {
+        TelaDeFilmes tela1 = new TelaDeFilmes();
+        tela1.setVisible(true);
+        this.dispose();
+    }
+
+
+}
