90aaf4f1fcfd1ee977c316b8f1d74d9b31174b82
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-01-08 23:22:59 -0300

    Conexão com o Banco de Dados

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,17 +1,4 @@
 import gui.*;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Main {
 
     public static void main(String[] args) {
