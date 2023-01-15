c20a8739110a785ced6f6ae549e68e2be56e0afa
TyagoSantos <107686385+TyagoSantos@users.noreply.github.com>
2023-01-14 21:18:41 -0300

    atualização

--- src/src/Main.java
+++ src/src/Main.java
@@ -10,31 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
 //
-//        FuncionarioData funcionario = new FuncionarioData();
-//        Administrador adm = new Administrador();
-//        try {
-//            funcionario.pullLanches();
-//
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//
-//
-//
-
-            Funcionario funcionario1 = new Funcionario();
-
-            FuncionarioData funcionario = new FuncionarioData();
-            Administrador adm = new Administrador();
-            try {
-                funcionario.pullLanches();
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
             new TelaInicial();
-            System.out.println(funcionario.nomeLanche1);
+            
 
 
 
@@ -44,9 +21,6 @@ public class Main {
 
 
 
-//        //new LancheADM();
-//        //new crudADM();
-//
 
 }
 }
